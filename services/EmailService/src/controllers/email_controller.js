f9e15f7acb2919e75a24cc1e000d655931317308
PhucNguyen <giaphuc972003@gmail.com>
2025-09-30 11:33:53 +0700

    commit 2

--- services/EmailService/src/controllers/email_controller.js
+++ services/EmailService/src/controllers/email_controller.js
@@ -4,7 +4,7 @@ async function sendEmailBooking(req, res) {
   try {
     const { account, bookingDetailInfo, payment } = req.body;
     console.log(`Send email to ${account?.user?.email}`);
-    const result = await emailService.sendEmailBooking( account, bookingDetailInfo, payment );
+    const result = await emailService.sendEmailBooking(account, bookingDetailInfo, payment);
     res.json(result);
   } catch (err) {
     res.status(500).json({ message: err.message });
